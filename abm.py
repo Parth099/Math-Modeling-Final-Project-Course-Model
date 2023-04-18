0b11c3f61c987418e0a46eb96b7f965769ea37bf
Parth099 <parthnpatel715@gmail.com>
2023-04-17 22:19:11 -0400

    shifted to oop in abm.py

--- abm.py
+++ abm.py
@@ -1,12 +1,17 @@
-from collections import defaultdict
-from Scheduling.Scheduler import Scheduler
-from Scheduling.Models.Student import Student
-from GraphGeneration.DAGGenerator import DAGGenerator
-from dataloader.CourseLoader import CourseDataLoader
+#! important
+# add this module to $path to allow python to find it later
 import sys
 import os
-# add this module to $path to allow python to find it later
 sys.path.insert(0, os.getcwd())
+
+from collections import defaultdict
+
+from GraphGeneration.DAGGenerator import DAGGenerator
+
+from dataloader.CourseLoader import CourseDataLoader
+
+from Scheduling.Scheduler import Scheduler
+from Scheduling.Models.Student import Student
 
 
 """
@@ -20,29 +25,45 @@ Questions an this ABM can answer:
     3. Effectness of "weed-out classess"
 """
 
+class CourseABM():
 
-COURSE_DATA_PATH = "./data/prereq.json"        # path to file to be load
-CDL = CourseDataLoader(COURSE_DATA_PATH)
-DAG = DAGGenerator(CDL.course_info, CDL.course_map)
+    def __init__(self, datapath: str, num_students: int) -> None:
+        COURSE_DATA_PATH = datapath        # path to file to be load
+        CDL = CourseDataLoader(COURSE_DATA_PATH)
+        self.DAG = DAGGenerator(CDL.course_info, CDL.course_map)
 
-students = [Student(CDL.course_map) for _ in range(2000)]
-courses = CDL.course_info
+        self.students = [Student(CDL.course_map) for _ in range(num_students)]
+        self.courses = CDL.course_info
+        self.scheduler = Scheduler(self.students, self.courses, CDL.course_map, CDL.grad_reqs)
 
-scheduler = Scheduler(students, courses, CDL.course_map, CDL.grad_reqs)
+        self.has_run = False
 
-# run semesters
-while not all([stu.is_finished for stu in students]):
-    scheduler.assign_classes()
-    scheduler.increment_semester()
+    # run semesters
+    def run(self, verbose=False):
+        while not all([stu.is_finished for stu in self.students]):
+            self.scheduler.assign_classes()
+            self.scheduler.increment_semester()
 
-# read semester data
-for semester in range(scheduler.get_highest_semester()):
-    pass_count, fail_count = scheduler.get_passing_and_failing_counts(semester)
-    labels = defaultdict(str)
+            if verbose: print("INCREMENT SEMESTER")
 
-    for key in pass_count:
-        _label = f'Passed: {pass_count[key]}\nFailed: {fail_count[key]}'
-        labels[key] = _label
+        self.has_run = True
+
+    def gen_graphs(self):
+
+        if not self.has_run:
+            raise RuntimeError("gen_graph called before run()")
+
+        for semester in range(self.scheduler.get_highest_semester()):
+            pass_count, fail_count = self.scheduler.get_passing_and_failing_counts(semester)
+            labels = defaultdict(str)
+
+            for key in pass_count:
+                _label = f'Passed: {pass_count[key]}\nFailed: {fail_count[key]}'
+                labels[key] = _label
 
 
-    DAG.draw_graph_via_PYG(labels).draw(f'./img/sample-{semester}.png')
+            self.DAG.draw_graph_via_PYG(labels).draw(f'./img/sample-{semester}.png')
+
+ABM = CourseABM("./data/prereq.json", 20)
+ABM.run()
+ABM.gen_graphs()
